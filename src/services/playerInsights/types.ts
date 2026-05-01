698d46207e711c8015979b591b847976df213f6f
aw249 <alexwoodbridge0303@gmail.com>
2026-05-01 16:17:54 +0100

    feat(playerInsights): output type definitions

---
+++ src/services/playerInsights/types.ts
@@ -0,0 +1,12 @@
+export interface UnitReadyState {
+  baseId: string;
+  name: string;
+  found: boolean;
+  rarity: number;
+  level: number;
+  gearLevel: number;
+  relicTier: number;
+  zetaCount: number;
+  omicronCount: number;
+  nextStepHint: string;
+}
